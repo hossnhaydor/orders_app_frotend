7a410431b3cfc66c38c84ed793278e5a73f36b61
hossnhaydor <syhossn@gmail.com>
2024-12-01 12:00:20 +0300

    test

--- lib/providers/user.dart
+++ lib/providers/user.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/material.dart';
-
-class User {
-  final String name;
-  final int id;
-  final String image;
-  final String phoneNumber;
-  final String location;
-  User({required this.id,required this.name,required this.name})
-}
-
-class UserProvider extends ChangeNotifier {}
+// import 'package:flutter/material.dart';
+//
+// class User {
+//   final String name;
+//   final int id;
+//   final String image;
+//   final String phoneNumber;
+//   final String location;
+//   User({required this.id,required this.name,required this.name})
+// }
+//
+// class UserProvider extends ChangeNotifier {}
